69891d7e9a91de5805a0852566ab5915f7461a98
Jukka Talja <jukka@talja.net>
2026-01-04 21:54:23 +0200

    Update __init__.py

--- custom_components/cozify_han/__init__.py
+++ custom_components/cozify_han/__init__.py
@@ -1,14 +1,23 @@
-"""Cozify HAN -integraatio."""
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from .const import DOMAIN
+
+DOMAIN = "cozify_han"
+PLATFORMS = ["sensor"]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Aseta integraatio käyttöliittymästä."""
-    # Tämä rivi käskee HA:ta lataamaan sensor.py:n
-    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+    """Aseta integraatio käyttöliittymästä lisätylle merkinnälle."""
+    hass.data.setdefault(DOMAIN, {})
+    
+    # Tallennetaan asennustiedot muistiin
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+
+    # Käynnistetään sensorit
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Poista integraatio."""
-    return await hass.config_entries.async_unload_platforms(entry, ["sensor"])
+    """Poista integraatio (esim. kun käyttäjä poistaa sen UI:sta)."""
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+    return unload_ok
