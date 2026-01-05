f1813760dc102cbda6caa138b1977de7ee756b1c
Jukka Talja <jukka@talja.net>
2026-01-05 19:58:13 +0200

    Update __init__.py

--- custom_components/cozify_han/__init__.py
+++ custom_components/cozify_han/__init__.py
@@ -4,9 +4,18 @@ from .const import DOMAIN
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Asetetaan integraatio config entryn perusteella."""
+    
+    # Rekisteröidään kuuntelija asetusten muutoksille
+    entry.async_on_unload(entry.add_update_listener(update_listener))
+
     # Välitetään asennus sensor-alustalle
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+    
     return True
+
+async def update_listener(hass: HomeAssistant, entry: ConfigEntry):
+    """Päivittää integraation kun asetuksia (Options) muutetaan."""
+    await hass.config_entries.async_reload(entry.entry_id)
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Poistetaan integraatio."""
