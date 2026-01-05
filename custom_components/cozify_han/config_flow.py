075510c3c1b8dae2dbd627757e915dbbd3fbaa38
Jukka Talja <jukka@talja.net>
2026-01-05 10:16:04 +0200

    Update config_flow.py

--- custom_components/cozify_han/config_flow.py
+++ custom_components/cozify_han/config_flow.py
@@ -1,17 +1,20 @@
-"""Asennusvalikko Cozify HAN -laitteelle."""
 import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.const import CONF_HOST
 from .const import DOMAIN
 
 class CozifyHanConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Käsittelee asennusvaiheet."""
+    """Cozify HAN asennusvalikko."""
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        """Kysy IP-osoitetta."""
+        """Pyydetään käyttäjältä IP-osoite."""
         if user_input is not None:
-            return self.async_create_entry(title="Cozify HAN Bridge", data=user_input)
+            # Luodaan entry. Title on se, mikä näkyy integraatiolistassa.
+            return self.async_create_entry(
+                title=f"Cozify HAN ({user_input[CONF_HOST]})", 
+                data=user_input
+            )
 
         return self.async_show_form(
             step_id="user",
